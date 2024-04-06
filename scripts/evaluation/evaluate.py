ddec0892871a84efbeeed46f8dc90e69cf0ff848
Yogesh Kalakoti <x_yogka@sigma.nsc.liu.se>
2024-04-06 10:39:27 +0200

    Evaluation is functional now

--- scripts/evaluation/evaluate.py
+++ scripts/evaluation/evaluate.py
@@ -1,4 +1,5 @@
 import argparse
+from pathlib import Path
 
 import tensorflow as tf
 import numpy as np
@@ -6,8 +7,43 @@ from numpy import newaxis
 from tqdm import tqdm
 import glob
 import pickle
-
+import matplotlib.pyplot as plt
 from sklearn.metrics import average_precision_score, precision_recall_curve, roc_auc_score, roc_curve
+
+class DataProcessor:
+    @staticmethod
+    def process_npy(data, alignment_max_depth):
+        features, labels = data['features'], data['labels']
+        # Process X
+        length = features.shape[0]
+        if length > 3000:
+            length = 3000
+        X = features[:length, :alignment_max_depth][np.newaxis, :] #.reshape(length * alignment_max_depth)[np.newaxis, :]
+
+        labels = np.reshape(labels[:length], (1, length, 1))
+        return X, labels
+    
+    @staticmethod
+    def count_steps(data_list):
+        count = 0
+        for target in data_list:
+            target = target.rstrip()
+            if Path(data_path, f"{target}.npy").exists():
+                count += 1
+        return count
+
+    @staticmethod
+    def generate_inputs_onego(data_list, alignment_max_depth):
+        for target in data_list:
+            target = target.rstrip()
+            target_path = Path(data_path, f'{target}.npy')
+            if target_path.exists():
+                data = np.load(target_path, allow_pickle=True).item()
+            else:
+                continue
+            X, labels = DataProcessor.process_npy(data, alignment_max_depth)
+
+            yield X, labels
 
 class CustomMetrics:
     @staticmethod
@@ -53,57 +89,89 @@ def parse_config(config_file):
         return config
 
 # Define command-line arguments
-parser = argparse.ArgumentParser(description='Train the model.')
-parser.add_argument('--config', type=str, default='config_eval.txt', help='Path to the config text file.')
+parser = argparse.ArgumentParser(description='Evaluate model')
+parser.add_argument('--config', type=str, default='config_eval.txt', help='Path to the eval config text file.')
 args = parser.parse_args()
 
 # Parse config file
 config = parse_config(args.config)
-print(config)
+print('\nEvaluationg on the following parameters...')
+for key, value in config.items():
+    print('>>', key, value)
 
 if __name__ == "__main__":
     # Load parameters from config file
     test_file = config.get('test_file')
     trained_models = config.get('trained_models')
     data_path = config.get('data_path')
+    out_path = config.get('out_path')
 
     # Load train, test, and validation data
     test_list = open(test_file).readlines()
 
     # Initialize result dict
     results={}
+    for model_path in glob.glob(trained_models+'/*'):
+        if 'mmseqs' in model_path:
+            print(model_path)
+            continue
 
-    for model_path in glob.glob(trained_models):
+        print('\nFound trained model:', model_path)
+        print(model_path.split('_'))
         ew_model = tf.keras.models.load_model(model_path, custom_objects={"true_positives": CustomMetrics.true_positives,
                                                                         "true_negatives": CustomMetrics.true_positives,
                                                                         "positives": CustomMetrics.true_positives,
                                                                         "negatives": CustomMetrics.true_positives,
                                                                         "balanced_acc": CustomMetrics.balanced_acc})
 
-        alignment_max_depth = int(model_path.split('depth_')[-1].split('_stage')[0])
-        labels_all = []
-        y_all = []
-        for target in tqdm(test_list):
+        alignment_max_depth = int(model_path.split('_')[-2])    # This has to be decided on
+        msa_tool = str(model_path.split('_')[-3])
+        labels_all, y_all = [], []
+        
+        print(f'Testing {len(test_list)} proteins')
+        for target in tqdm(test_list, ncols=100):
             target = target.rstrip()
-            data = np.load(f'{data_path}{target}.npy', allow_pickle=True).item()
-            features, labels = data['features'], data['labels']
+            target_path = Path(data_path, f"{target}.npy")
+            if target_path.exists():
+                data = np.load(target_path, allow_pickle=True).item()
+            else:
+                continue
 
-            # Process X
-            length = features.shape[0]
-            X = features[:, :alignment_max_depth].reshape(length * alignment_max_depth)[newaxis, :]
-
-            # Process Y
-            labels_ = labels[np.newaxis, :]
-            labels_ = np.reshape(labels_, (1, labels_.shape[1], 1))
+            X, labels = DataProcessor.process_npy(data, alignment_max_depth)
             y = ew_model.predict(X)
 
-            labels_all.append(labels_.flatten())
-            y_all.append(y[0][:,1])
+            labels_all.append(labels.flatten())
+            y_all.append(y[0][:, 1])
 
         labels_all_arr = np.concatenate(labels_all)
         y_all_arr = np.concatenate(y_all)
 
         results[f'rawmsa_{alignment_max_depth}']={'labels':labels_all_arr, 'y_all':y_all_arr}
 
-    with open('filename.pickle', 'wb') as handle:
+    with open(f'{out_path}/evaluation_results.pickle', 'wb') as handle:
         pickle.dump(results, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    
+    print(results.keys())
+    
+    fig, ax = plt.subplots(1,2,figsize=(10,5))
+    for key in results.keys():
+        labels_all_arr, y_all_arr = results[key]['labels'], results[key]['y_all']
+        pr, re, _ = precision_recall_curve(labels_all_arr, y_all_arr)
+        aupr = np.round(average_precision_score(labels_all_arr, y_all_arr),2)
+        fpr, tpr, thresholds = roc_curve(labels_all_arr, y_all_arr, pos_label=1)
+        auroc = str(np.round(roc_auc_score(labels_all_arr, y_all_arr),2))
+
+        ax[1].plot(re, pr, label=key.split('/')[-1]+f' ({aupr})')
+        ax[0].set_xlabel('FPR')
+        ax[0].set_ylabel('TPR')
+        ax[0].plot(fpr, tpr, label=key.split('/')[-1]+f' (auroc:{auroc}, aupr: {aupr})')
+        ax[1].set_xlabel('Recall')
+        ax[1].set_ylabel('Precision')
+        #ax[0].legend(fontsize=8)
+    #average_precision_score(labels_all_arr, y_all_arr)
+
+    handles, labels = ax[0].get_legend_handles_labels()
+    fig.legend(handles, labels, bbox_to_anchor=(0.75, 1.15))
+    #plt.legend()
+    plt.tight_layout()
+    fig.savefig(f'{out_path}/evaluation_results.pdf', bbox_inches='tight')
